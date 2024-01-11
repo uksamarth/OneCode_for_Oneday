ea4c323870b8357fcee8917b19c13e741fb248a7
U K Samarth <105077462+uksamarth@users.noreply.github.com>
2024-01-11 23:19:15 +0530

    Create reverse_array.cpp

---
+++ reverse_array.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+	// your code goes here
+	int n;
+	cin>> n;
+	int a[n],i;
+	for (i=0;i<n;++i)
+	cin>>a[i];
+	
+	int j=n-1;
+	i=0;
+	while(i<j)
+	{
+	    swap(a[i],a[j]);
+	    i++;
+	    j--;
+	}
+	for(i=0;i<n;++i)
+	cout<<a[i]<<" ";
+
+}
